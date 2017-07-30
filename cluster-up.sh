55560ae29c538968df98c8aee771c4e06cd4f6ce
sbouii <mariemsbouiii40@gmail.com>
2017-07-30 23:40:07 +0100

    first commit

---
+++ cluster-up.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##Create infrastructure and inventory file
+echo "Creating infrastructure"
+vagrant up
+create_inventory_file.sh
+##Run Ansible playbooks
+echo "Quick sleep while instances spin up"
+sleep 120
+echo "Ansible provisioning"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i kubespray/inventory/inventory -u vagrant -b kubespray/cluster.yml
