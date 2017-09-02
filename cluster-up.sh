f32eb80c243568deb848fd62978d168338e81b49
sbouii <mariem.sbouiii40@gmail.com>
2017-09-02 23:11:43 +0100

    Update cluster-up.sh

--- cluster-up.sh
+++ cluster-up.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-##Create infrastructure and inventory file
+##Create infrastructure 
 echo "Creating infrastructure"
 cd kubespray
 vagrant up
-##Run Ansible playbooks
-echo "Quick sleep while instances spin up"
-sleep 120
-echo "Ansible provisioning"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i kubespray/inventory/inventory -u vagrant -b kubespray/cluster.yml
